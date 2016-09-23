045c6ece8eeca4717a665324b4337551af9c3fbc
Kavanagh, Luke <ltkavanagh@gmail.com>
2016-09-23 12:19:43 +0100

    Add more concept vanities

--- src/server/lib/resolve-id.js
+++ src/server/lib/resolve-id.js
@@ -1,6 +1,11 @@
 const vanities = {
 	thing: {
 		news: 'Nw==-R2VucmVz',
+		baseline: 'N2M5Mjc5ZjktNjBhOS00MjEzLWFjMjItMGVhNjNiN2U1MTFj-QnJhbmRz',
+		companies: 'Mjk&#x3D;-U2VjdGlvbnM&#x3D;',
+		world: 'MQ&#x3D;&#x3D;-U2VjdGlvbnM&#x3D;',
+		'chart-doctor': 'YTIwMWFhZGEtOGE4MS00ZDdlLTlhYjUtZjA4NmY0ZjcxNzhh-QnJhbmRz',
+		'the-big-read': 'MTE4-U2VjdGlvbnM&#x3D;',
 	},
 
 	list: {
