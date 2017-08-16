24c28eb1151924e8b34ed08049fc00223cc2e1b6
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-08-16 18:18:34 +0100

    Fixes a few issues with the v1 endpoint

--- src/server/helpers.js
+++ src/server/helpers.js
@@ -33,6 +33,3 @@ export function strftime(date, format = defaultFTDateFormat) {
 export function ftdate(d) {
 	return strftime(d);
 }
-
-export const isV1Id = str => /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/
-	.test(str);
--- src/server/lib/load-thing-v1.js
+++ src/server/lib/load-thing-v1.js
@@ -1,9 +1,10 @@
-import * as fetch from 'node-fetch';
+import fetch from 'node-fetch';
 import createError from 'http-errors';
 import api from '@financial-times/n-es-client';
 import list from '../models/list';
-import { isV1Id } from '../helpers';
 import { getThings } from './load-thing';
+
+const { CONCORDANCE_API_KEY } = process.env;
 
 /**
  * Downloads a list of articles from CAPI and returns
@@ -11,57 +12,54 @@ import { getThings } from './load-thing';
  */
 export default async function loadThingV1(id) {
 	try {
-		if (isV1Id(id)) {
-			const endpoint = `http://api.ft.com/concordances?identifierValue=${id}=&authority=http://api.ft.com/system/FT-TME&apiKey=${process.env.CONTENT_API_KEY}`;
-			const { concordances } = (await (await fetch(endpoint)).json());
-			const idV2 = concordances[0].concept.id.replace(/https?:\/\/api\.ft\.com\/\w+\//, '');
+		const endpoint = `http://api.ft.com/concordances?identifierValue=${id}&authority=http://api.ft.com/system/FT-TME&apiKey=${CONCORDANCE_API_KEY}`;
+		const { concordances } = (await (await fetch(endpoint)).json());
+		const idV2 = concordances[0].concept.id.replace(/https?:\/\/api\.ft\.com\/\w+\//, '');
 
-			return Promise.all([
-				api.search({
-					query: {
-						term: { 'annotations.id': idV2 },
-					},
-				}),
+		return Promise.all([
+			api.search({
+				query: {
+					term: { 'annotations.id': idV2 },
+				},
+			}),
 
-				getThings([idV2]),
-			])
-				.then(([searchResults, tags]) => {
-					if (!tags.items || !tags.items.length) {
+			getThings([idV2]),
+		])
+			.then(([searchResults, tags]) => {
+				if (!tags.items || !tags.items.length) {
+					throw new createError(404, ''); // Empty response to prevent "Not Found" text
+				}
+
+				return list({
+					id,
+					type: tags.items[0].taxonomy,
+					items: searchResults,
+					title: tags.items[0].name,
+					canFollow: true,
+					url: 'https://www.ft.com/stream/' + id,
+				});
+			})
+			.catch((err) => {
+				// workaround api client rejecting with a stackless error
+				// - see https://github.com/matthew-andrews/fetchres/issues/9
+				if (!(err instanceof Error) || !err.stack) {
+					if (err.name === 'BadServerResponseError') {
 						throw new createError(404, ''); // Empty response to prevent "Not Found" text
 					}
 
-					return list({
-						id,
-						type: tags.items[0].taxonomy,
-						items: searchResults,
-						title: tags.items[0].name,
-						canFollow: true,
-						url: 'https://next.ft.com/stream/' + tags.items[0].taxonomy + 'Id/' + id,
-					});
-				})
-				.catch((err) => {
-					// workaround api client rejecting with a stackless error
-					// - see https://github.com/matthew-andrews/fetchres/issues/9
-					if (!(err instanceof Error) || !err.stack) {
-						if (err.name === 'BadServerResponseError') {
-							throw new createError(404, ''); // Empty response to prevent "Not Found" text
-						}
-
-						const nonError = new Error(`
-						Non-error thrown -
-							name: "${err.name}";
-							message: "${err.message}"}
-					`);
-						nonError.originalError = err;
-						throw nonError;
-					}
-					throw err;
-				});
-		}
+					const nonError = new Error(`
+					Non-error thrown -
+						name: "${err.name}";
+						message: "${err.message}"}
+				`);
+					nonError.originalError = err;
+					throw nonError;
+				}
+				throw err;
+			});
 	}
 	catch (e) {
+		console.error(e);
 		throw e;
 	}
-
-	return false;
 }
