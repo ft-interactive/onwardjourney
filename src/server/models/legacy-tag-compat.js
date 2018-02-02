ae1a2085a5977954dd9ed97c276f146e0b4b4b54
Luke Kavanagh <kavanagh@users.noreply.github.com>
2018-02-02 22:25:11 +0000

    Fix stream Url

--- src/server/models/legacy-tag-compat.js
+++ src/server/models/legacy-tag-compat.js
@@ -1,5 +1,5 @@
 const streamUrl = (id, taxonomy) =>
-	(id && taxonomy ? `https://next.ft.com/stream/${taxonomy}Id/${id}` : '');
+	(id && taxonomy ? `https://www.ft.com/stream/${taxonomy}Id/${id}` : '');
 
 const createTag = ({ taxonomy, headshot, prefLabel, idV1 }) =>
 	({ id: idV1, taxonomy, headshot, name: prefLabel, url: streamUrl(idV1, taxonomy) });
